6257ba158474d2bd34911ca326a835f079093493
Tung Yeung <oxcdbe@gmail.com>
2026-07-06 16:10:27 +0800

    test

---
+++ src/BodyAreaDevelop.tsx
@@ -0,0 +1,117 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import BodyAreaLeft from './BodyAreaWritingsLeft';
+import BodyAreaRight from './BodyAreaWritingsRight';
+import { BodyAreaGoTo, Direction } from './BodyAreaGoTo';
+import dataHub from './DataHub';
+import { buildAppRouteUrl } from './AppRouteParameters';
+
+
+function BodyAreaDevelop() {
+  const [isLeftFinishedTrans, setIsLeftFinishedTrans] = useState(true);
+  const [isRightFinishedTrans, setIsRightFinishedTrans] = useState(true);
+
+  const [writingsMode, setWritingsMode] = useState('POSTS');
+  dataHub.addListener(setWritingsMode, 'WRITINGS_MODE');
+  const [aritcleCateSelected, setAritcleCateSelected] = useState(788875233);
+  dataHub.addListener(setAritcleCateSelected, 'ARTICLE_CATE_SELECTED');
+  
+  const navigate = useNavigate();
+
+  const isAtLeftArea = (writingsMode != 'ARTICLES');
+
+  const area2 = 100;
+  const area3px = 40;
+
+  const templateColumns = (isAtLeftArea) ? `calc(${area2}% - ${area3px}px) ${area3px}px`
+                                        : `${area3px}px calc(${area2}% - ${area3px}px)`;
+
+
+  return (
+    <>
+        <div style={{
+          display: 'grid',
+          gridTemplateColumns: templateColumns,
+          transition: 'grid-template-columns 0.5s ease',
+          height: '100%',
+        }}>
+          {/* Left Area */}
+          <div 
+            onTransitionStart={()=>{
+              setIsLeftFinishedTrans(false)
+            }}
+            
+            onTransitionEnd={()=>{
+              setIsLeftFinishedTrans(true)
+            }}
+          >
+
+            <div style={{
+                display: (isAtLeftArea && isLeftFinishedTrans) ? 'inline' : 'none',
+                height: '100%',
+                width: 'calc(100% - 20px)',
+              }}
+            >
+              <BodyAreaLeft/>
+            </div>
+            <div style={{
+                display: (isAtLeftArea && isLeftFinishedTrans) ? 'none' : 'inline',
+                height: '100%',
+                width: 'calc(100% - 20px)',
+              }}
+
+              onMouseUp={()=>{
+                navigate(buildAppRouteUrl({ navi: 'WRITINGS', writingsMode: 'POSTS' }));
+              }}
+            >
+              <BodyAreaGoTo p_direction={Direction.LEFT}/>
+            </div>
+        </div>
+
+          {/* Right Area */}
+          <div  
+            style={{
+              height: '100%',
+              width: '100%',
+            }}
+            onTransitionStart={(e)=>{
+              if(e.target != e.currentTarget) return;
+              setIsRightFinishedTrans(false)
+            }}
+            
+            onTransitionEnd={(e)=>{
+              if(e.target != e.currentTarget) return;
+              setIsRightFinishedTrans(true)
+            }}
+          >
+            <div style={{
+                display: (!isAtLeftArea && isRightFinishedTrans) ? 'block' : 'none',
+                height: '100%',
+                width: 'calc(100% - 20px)',
+              }}
+            >
+              <BodyAreaRight/>
+            </div>
+            <div style={{
+                display: (!isAtLeftArea && isRightFinishedTrans) ? 'none' : 'inline',
+                height: '100%',
+                width: 'calc(100% - 20px)',
+              }}
+
+              onMouseUp={()=>{
+                navigate(buildAppRouteUrl({
+                  navi: 'WRITINGS',
+                  writingsMode: 'ARTICLES',
+                  cateId: aritcleCateSelected,
+                }));
+              }}
+            >
+              <BodyAreaGoTo p_direction={Direction.RIGHT}/>
+            </div>
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default BodyAreaDevelop;
